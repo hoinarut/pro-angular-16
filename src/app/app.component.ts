f4ff9aff6d6deb81a51e9d5950b96e81b437e852
hoinarut-cv <tudor.hoinaru@code-ventures.ro>
2023-08-30 09:01:16 +0300

    Chapter 2 WIP

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import {Component} from '@angular/core';
+import {TodoList} from "./todoList";
+import {TodoItem} from "./todoItem";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private list = new TodoList("Tudor", [
+    new TodoItem("Enel deploy", true),
+    new TodoItem("HSO management dev time"),
+    new TodoItem("HSO File Upload")
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items
+      .filter(item => !item.complete).length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items;
+  }
+}
